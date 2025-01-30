b918a5981e3c5e0a5babfb87f5a494d12efae79a
eaa24fnie <eaa24fnie@students.eaaa.dk>
2025-01-30 12:13:36 +0100

    DONE

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
 "use script";
 
+function onBulb() {
+    const bulb = document.getElementById("bulb");
+    bulb.src = "img/on.jpg";
+  }
+
+  function offBulb() {
+    const bulb = document.getElementById("bulb");
+    bulb.src = "img/off.jpg";
+  }
+
+  
